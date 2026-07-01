113759ad4fdabbd50772388895ea9e52bfc6d539
Mauro Lencinas <lencinasmauro448@gmail.com>
2026-06-30 21:17:23 -0300

    Creación de index.html y configuracion de mensaje en views.py

--- Proyecto1/myapp/views.py
+++ Proyecto1/myapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+def index(request):
+    context = { "mensaje": "¡Bienvenidos a mi primera app con Django!" }
+    return render(request, 'myapp/index.html', context)
 # Create your views here.
