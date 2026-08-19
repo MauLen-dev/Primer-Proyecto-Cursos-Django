f1f7fe59b7304460923e863db4de338076c984ec
Mauro Lencinas <lencinasmauro448@gmail.com>
2026-08-18 22:51:41 -0300

    definicion de funciones  "estudiantes y estudiantes" en views.py

--- Proyecto1/myapp/views.py
+++ Proyecto1/myapp/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Estudiante, Profesor, Curso, Entregable
 
 def index(request):
     context = { "mensaje": "¡Bienvenidos a mi primera app con Django!" }
     return render(request, 'myapp/index.html', context)
+
+def lista_estudiantes(request):
+    estudiantes = Estudiante.objects.all()
+    return render(request, 'myapp/lista_estudiantes.html', {'estudiantes': estudiantes})
+
+def detalle_estudiante(request, pk):
+    estudiante = get_object_or_404(Estudiante, pk=pk)
+    return render(request, 'myapp/detalle_estudiante.html', {'estudiante': estudiante})
+
 # Create your views here.
