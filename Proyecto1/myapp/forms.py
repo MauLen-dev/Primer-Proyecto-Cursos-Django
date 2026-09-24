0c072f6eef75d80f96c7e52d2ca29dece14b2dd1
Mauro Lencinas <lencinasmauro448@gmail.com>
2026-09-24 01:24:35 -0300

    creacion de formulario de profesores

--- Proyecto1/myapp/forms.py
+++ Proyecto1/myapp/forms.py
@@ -3,4 +3,10 @@ from django import forms
 class CursoForm(forms.Form):
     nombre = forms.CharField(max_length=100)
     camada= forms.IntegerField()
-    
+
+class ProfesorFormulario(forms.Form):
+   nombre = forms.CharField(max_length=10, label="Nombre")
+   apellido = forms.CharField(max_length=100, label="Apellido")
+   email = forms.EmailField(label="Correo electrónico")
+   profesion = forms.CharField(max_length=100, label="Profesion")
+        
--- Proyecto1/myapp/views.py
+++ Proyecto1/myapp/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Estudiante, Profesor, Curso, Entregable
-from .forms import CursoForm
+from .forms import CursoForm, ProfesorFormulario
 
 def index(request):
     context = { "mensaje": "¡Bienvenidos a mi primera app con Django!" }
@@ -38,5 +38,21 @@ def cursoform(request):
             return render(request, 'myapp/curso_exito.html')
     else:
         form = CursoForm()
-    return render(request, 'myapp/curso_formulario.html', {'form': form})        
+    return render(request, 'myapp/curso_formulario.html', {'form': form})  
+
+def profesorFormulario(request):
+   if request.method == 'POST':
+     form = ProfesorFormulario(request.POST) 
+     if form.is_valid():
+      nombre = form.cleaned_data['nombre']
+      apellido = form.cleaned_data['apellido']
+      email = form.cleaned_data['email']
+      profesion = form.cleaned_data['profesion']
+      profesor = Profesor(nombre=nombre, apellido=apellido, email=email, profesion=profesion)
+      profesor.save()
+      return redirect('profesores')
+   else:
+    form = ProfesorFormulario() 
+   return render(request, 'myapp/profesor_formulario.html', {'form': form})
+              
 # Create your views here.
