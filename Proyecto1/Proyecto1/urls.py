09ae1b749a3df23a7f96f63a7d5c0c43169a0aa5
Mauro Lencinas <lencinasmauro448@gmail.com>
2026-09-24 03:27:35 -0300

    minor fix

--- Proyecto1/Proyecto1/urls.py
+++ Proyecto1/Proyecto1/urls.py
@@ -30,5 +30,5 @@ urlpatterns = [
     path('detalle_estudiante/<int:pk>/', views.detalle_estudiante, name="detalle_estudiante"),
     path('cursoform/', views.cursoform, name="cursoform"),
     path('profesorFormulario/', views.profesorFormulario, name="profesorFormulario"),
-    path('profesor_editar/', views.profesor_editar, name="profesor_editar"),
+    path('profesor/editar/<int:id>', views.profesor_editar, name="profesor_editar"),
 ]
